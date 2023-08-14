cd9a377c2750d7d6ee428a07c014530700403165
wangechimk <wangechik.michelle@gmail.com>
2023-08-14 23:42:20 +0300

    adding the start timer functions

--- script.js
+++ script.js
@@ -17,4 +17,62 @@ window.onload = () => {
     
 	workTittle.classList.add('active');
     }
+
+   // start timer
+function start() {
+	// change button
+	document.getElementById('start').style.display = "none";
+	document.getElementById('reset').style.display = "block";
     
+	// change the time
+	seconds = 59;
+    
+	let workMinutes = workTime - 1;
+	let shortbreakMinutes =workTime -1;
+	let longbreakMinutes =workTime -1;
+
+    
+	breakCount = 0;
+   
+// countdown
+let timerFunction = () => {
+        //change the display
+        document.getElementById('minutes').innerHTML = workMinutes;
+	document.getElementById('minutes').innerHTML = shortbreakMinutes;
+	document.getElementById('minutes').innerHTML = longbreakMinutes;
+        document.getElementById('seconds').innerHTML = seconds;
+
+	// start
+        seconds = seconds - 1;
+
+        if(seconds === 0) {
+            workMinutes = workMinutes - 1;
+            if(workMinutes === -1 ){
+                if(breakCount % 2 === 0) {
+                    // start break
+                    workMinutes = longbreakMinutes;
+                    breakCount++
+		    // start break
+                    workMinutes = shortbreakMinutes;
+                    breakCount++
+
+                    // change the buttons__block
+                    workTittle.classList.remove('active');
+                    breakTittle.classList.add('active');
+                }else {
+                    // continue work
+                    workMinutes = workTime;
+                    breakCount++
+
+                    // change the buttons__block
+                    breakTittle.classList.remove('active');
+                    workTittle.classList.add('active');
+                }
+            }
+            seconds = 59;
+        }
+    }
+
+    // start countdown
+    setInterval(timerFunction, 1000); // 1000 = 1s
+}
